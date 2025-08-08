feaaa35e783d6369bb352e3075d860f7a17dd630
kqesar <yoann.fievez@gmail.com>
2025-08-08 22:44:05 +0200

    build: update config files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteCompression from "vite-plugin-compression";
-
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/alltp-tracker/" : "/",
